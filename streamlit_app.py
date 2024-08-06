d8ac74473aacf14fe702ae0c694c1ebb4337e219
Edgardo Hernandez <157829083+hermondez@users.noreply.github.com>
2024-08-05 20:03:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,7 @@ if ingredients_list:
     
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
+        st.subheader(fruit_choosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
